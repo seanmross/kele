ec0aff597f2284c48a0243118c123ea9c93378ce
Sean Ross <seanmross9@gmail.com>
2017-07-04 12:44:05 -0400

    User-story-3: complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,8 +7,8 @@ class Kele
 
   def initialize(email, password)
     response = self.class.post("#{BASE_URI}/sessions", body: { "email": email, "password": password })
-    puts response.code
-    raise "Invalid email or password" if response.code == 404
+    JSON.parse(response.body)
+    raise "Invalid email or password" if response.code != 200
     @auth_token = response["auth_token"]
   end
 
@@ -16,4 +16,10 @@ class Kele
     response = self.class.get("#{BASE_URI}/users/me", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("#{BASE_URI}/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
+    @mentor_availability = JSON.parse(response.body)
+  end
+
 end
