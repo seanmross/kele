d504970dc3dfea3085e91cf45fe3be15fe119029
Sean Ross <seanmross9@gmail.com>
2017-07-06 23:13:27 -0400

    user-story-6 updated with enrollment_id

--- lib/kele.rb
+++ lib/kele.rb
@@ -44,7 +44,7 @@ class Kele
   end
 
   def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
-    response = self.class.post("#{BASE_URI}/checkpoint_submissions", body: { "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment }, headers: { "authorization" => @auth_token })
+    response = self.class.post("#{BASE_URI}/checkpoint_submissions", body: { "enrollment_id": 22463, "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment }, headers: { "authorization" => @auth_token })
     raise "Invalid submission" if response.code != 200
     puts response
   end
